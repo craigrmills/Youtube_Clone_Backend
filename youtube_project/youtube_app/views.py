7255b3a899e72e64bde6de43591fdf29823ec5d0
craigrmills <craig.r.mills86@gmail.com>
2021-08-18 13:11:12 -0500

    Testing gitignore

--- youtube_project/youtube_app/views.py
+++ youtube_project/youtube_app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from .models import Comment, Reply
+from .serializers import CommentSerializer, ReplySerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from django.http import Http404
 
 # Create your views here.
+class CommentList(APIView):
+    def get(self, request):
+        reply = Reply.objects.all()
+        serializer = ReplySerializer(reply, many=True)
+        return Response(serializer.data)
+
+# Comment for a push...
